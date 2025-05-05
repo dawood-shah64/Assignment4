9e573f90ac19e80abba06a2e6a363cdfa2b15a29
dawood-shah64 <helexalpha334@gmail.com>
2025-05-05 12:20:37 +0530

    Delete task1.py

--- task1.py
+++
@@ -1,7 +0,0 @@
-try:
-    with open('sample.txt', "r") as file:
-        print('reading file content:')
-        print("line 1: ", file.readline())
-        print("line 2: ", file.readline())
-except FileNotFoundError:
-    print("The file", file_name, "was not found.")
